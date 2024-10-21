053c2802c25fdcf4ec10430a8da24a21bf3d93e9
acataide <acataide@gmail.com>
2024-10-21 11:42:40 -0300

    Adição de formulario de edição de usuarios

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,22 @@ var router = express.Router();
 
 var Usuarios = require('../models/usuarios');
 
+router.get('/detalhe/:id', async function(req, res, next){
+  var user = await Usuarios.findOne({_id:req.params.id}).exec();
+  res.render('usuarios_detalhe', {user: user});
+});
+
+router.post('/editar', async function(req, res, next){
+  res.send(req.body.name);
+});
+
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  var usuarios = await Usuarios.find().exec();
-  res.render('usuarios', { title: 'Usuários', usuarios: usuarios });
+router.get('/:page', async function(req, res, next) {
+  var page = parseInt(req.params.page);
+  var usuarios = await Usuarios.find().skip(10 * page).limit(10).exec();
+  res.render('usuarios', { title: 'Usuários', 
+                           usuarios: usuarios,
+                           page: page });
 });
 
 module.exports = router;
