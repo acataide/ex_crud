398e5ce601c51a77192043a7120a9f05c92a72f0
acataide <acataide@gmail.com>
2024-11-04 09:51:15 -0300

    Implementação de CREATE e DELETE

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
 var Usuarios = require('../models/usuarios');
 
 router.get('/detalhe/:id', async function(req, res, next){
   var user = await Usuarios.findOne({_id:req.params.id}).exec();
-  res.render('usuarios_detalhe', {user: user});
+  res.render('usuarios_detalhe', {user: user, title:'Editar Usuário'});
 });
 
 router.post('/editar', async function(req, res, next){
-  var user = await Usuarios.findOne({_id:req.body.id}).exec();
+  if (req.body.novo === 'true') {
+    var user = new Usuarios();
+  } else {
+    var user = await Usuarios.findOne({_id:req.body.id}).exec();
+  }
   user.name = req.body.name;
   user.email = req.body.email;
   user.boolean = req.body.boolean === 'on';
   user.numberrange = req.body.numberrange;
   await user.save()
+  res.redirect('/users/0');
+});
+
+router.get('/novo', async function(req, res, next){
+  user = new Usuarios();
+  res.render('usuarios_detalhe', {user: user, title:'Novo Usuário'});
+});
+
+router.get('/apagar/:id', async function(req, res, next){
+  Usuarios.deleteOne({_id:req.params.id}).exec();
   res.redirect('/users/0');
 });
 
